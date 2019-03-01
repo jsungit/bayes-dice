8d7fb4fdf5c86f2b15ccfda79424e4d3cc6783c1
Jane Sun <jane.sun@supplylogx.io>
2019-03-01 16:12:22 -0600

    add in dev brach

--- src/bayesdice.py
+++ src/bayesdice.py
@@ -1,15 +1,28 @@
 import random
+import scipy as scs
 
 
 class BayesDice:
     def __init__(self):
-        pass
+        self.dice = [4, 6, 8, 12, 20]
+        self.data = {die: 0.20 for die in self.dice}
 
     def choose_die(self):
-        pass
+        self.die = random.choice(self.dice)
 
     def roll_die(self):
-        pass
+        return random.randint(1, self.die)
 
     def update_priors(self, roll):
-        pass
+        print('before update:', self.data)
+        denominator = list(map(lambda die: (0 if roll > die else (1 / die)) * self.data[die], self.dice))
+        self.data = {self.dice[i]: numerator / sum(denominator) for i, numerator in enumerate(denominator)}
+        self.debug(roll, denominator)
+
+    def debug(self, roll, denominator):
+        print('die:', self.die, 'roll:', roll)
+        print('denominator:', denominator)
+        print('data:', self.data)
+        print('sum of priors:', sum(self.data.values()))
+        print('sum of denominator:', sum(denominator))
+        print('-' * 50)
